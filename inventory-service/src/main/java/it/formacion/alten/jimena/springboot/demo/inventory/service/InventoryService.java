4afd3029c21bc3fd20f75ebce32cf5f6f6d29b5e
DraSusanCalvin <51798631+DraSusanCalvin@users.noreply.github.com>
2026-09-24 15:12:28 +0000

    Service + Impl + Response

---
+++ inventory-service/src/main/java/it/formacion/alten/jimena/springboot/demo/inventory/service/InventoryService.java
@@ -0,0 +1,11 @@
+package it.formacion.alten.jimena.springboot.demo.inventory.service;
+
+import java.util.List;
+
+import it.formacion.alten.jimena.springboot.demo.inventory.api.adapters.InventoryResponse;
+
+public interface InventoryService {
+
+    List<InventoryResponse> getInventario();
+    InventoryResponse getStock(Long productId);
+}
---
+++ inventory-service/src/main/java/it/formacion/alten/jimena/springboot/demo/inventory/service/InventoryServiceImpl.java
@@ -0,0 +1,43 @@
+package it.formacion.alten.jimena.springboot.demo.inventory.service;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import it.formacion.alten.jimena.springboot.demo.inventory.api.adapters.InventoryResponse;
+import it.formacion.alten.jimena.springboot.demo.inventory.entities.Inventory;
+import it.formacion.alten.jimena.springboot.demo.inventory.repository.InventoryRepository;
+
+public class InventoryServiceImpl implements InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    public InventoryServiceImpl(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+
+    private InventoryResponse toResponse(Inventory itemInventario) {
+        return new InventoryResponse(itemInventario.getProductId(),
+                itemInventario.getStock());
+    }
+
+    @Override
+    public List<InventoryResponse> getInventario() {
+        List<InventoryResponse> lista = inventoryRepository.findAll().stream()
+                .map((itemInventario) -> toResponse(itemInventario))
+                .toList();
+
+        return lista;
+    }
+
+    @Override
+    public InventoryResponse getStock(Long productId) {
+        Inventory itemInventory = inventoryRepository.findById(productId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No se encuentra el producto " + productId));
+
+        return toResponse(itemInventory);
+
+    }
+
+}
